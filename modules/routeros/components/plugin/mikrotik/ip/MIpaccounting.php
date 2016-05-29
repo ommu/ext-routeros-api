610d2edc447f9785b274b2f5425478f4a13684ba
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-05-29 12:44:26 +0700

    ip_accounting fixed

--- modules/routeros/components/plugin/mikrotik/ip/MIpaccounting.php
+++ modules/routeros/components/plugin/mikrotik/ip/MIpaccounting.php
@@ -2,6 +2,14 @@
 /**
  * Description of Mapi_Ip_Accounting
  *
+ * TOC :
+ *	get_all_accounting
+ *	set_accounting
+ *	get_all_web_access
+ *	set_web_access
+ *	get_all_snapshot
+ *	get_all_uncounted 
+ *
  * @author Putra Sudaryanto <putra.sudaryanto@gmail.com> <putra@sudaryanto.id>
  * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
  * @created date 26 May 2016, 15:15 WIB
@@ -23,106 +31,88 @@ class MIpaccounting {
 	}
 	
 	/**
-	 * This method is used to set or edit ip accountng
-	 * @param type $account_local_traffic string
-	 * @param type $enabled string
-	 * @param type $threshold string
-	 * @return type array
-	 */
-	public function set_accounting($account_local_traffic, $enabled, $threshold) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/accounting/set");
-		$sentence->setAttribute("account-local-traffic", $account_local_traffic);
-		$sentence->setAttribute("enabled", $enabled);
-		$sentence->setAttribute("threshold", $threshold);
-		$this->talker->send($sentence);
-		return "Sucsess";
-	}
-	
-	 /**
 	 * This method is used to display all accounting
 	 * @return type array
-	 * 
 	 */
 	public function get_all_accounting() {
-		$sentence = new SentenceUtil();
-		$sentence->fromCommand('/ip/accounting/getall');
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0;
-		if ($i < $rs->size()){
-			return $rs->getResultArray();
-		}else{
+		$array = $this->_conn->comm('/ip/accounting/getall');
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
 			return "No Ip Accounting To Set, Please Your Add Ip Accounting";
-		}
 	}
 	
-	 /**
-	 * This method is used to display all snapshot
+	/**
+	 * This method is used to set or edit ip accounting
+	 * @param
+	 *	enabled (yes |no; Default: no): whether local IP traffic accounting is enabled
+	 *	account-local-traffic (yes |no; Default: no): whether to account the traffic to/from the router itself
+	 *	threshold (integer; Default: 256): maximum number of IP pairs in the accounting table (maximal value is 8192)
+	 *
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Accounting#Local_IP_Traffic_Accounting
+	 *
 	 * @return type array
-	 * 
 	 */
-	public function get_all_snapshot() {
-		$sentence = new SentenceUtil();
-		$sentence->fromCommand('/ip/accounting/snapshot/getall');
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0;
-		if ($i < $rs->size()){
-			return $rs->getResultArray();
-		}else{
-			return "No Ip Accounting Snapshot To Set, Please Your Add Ip Accounting Snapshot";
-		}
+	public function set_accounting($param) {
+		$this->_conn->comm("/ip/accounting/set", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
-	 /**
-	 * This method is used to display all uncounted
-	 * @return type array
-	 * 
-	 */
-	public function get_all_uncounted() {
-		$sentence = new SentenceUtil();
-		$sentence->fromCommand('/ip/accounting/uncounted/getall');
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0;
-		if ($i < $rs->size()){
-			return $rs->getResultArray();
-		}else{
-			return "No Ip Accounting Uncounted To Set, Please Your Add Ip Accounting Uncounted";
-		}
-	}
-	
-	 /**
+	/**
 	 * This method is used to display all web-acces
 	 * @return type array
-	 * 
 	 */
 	public function get_all_web_access() {
-		$sentence = new SentenceUtil();
-		$sentence->fromCommand('/ip/accounting/web-access/getall');
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0;
-		if ($i < $rs->size()){
-			return $rs->getResultArray();
-		}else{
+		$array = $this->_conn->comm("/ip/accounting/web-access/getall");
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
 			return "No Ip Accounting web-access To Set, Please Your Add Ip Accounting web-access";
-		}
 	}
+	
 	/**
 	 * This method is used to ip accounting set web-acces
-	 * @param type $accessible_via_web string default : yes or no
+	 * @param
+	 *	accessible-via-web (yes | no; Default: no): whether the snapshot is available via web
+	 *	address (IP address/netmask; Default: 0.0.0.0/0): IP address range that is allowed to access the snapshot	
+	 *
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Accounting#Web_Access_to_the_Local_IP_Traffic_Accounting_Table
+	 *
 	 * @return type array
-	 * 
 	 */
-	public function set_web_access($accessible_via_web) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/accounting/web-access/set");
-		$sentence->setAttribute("accessible-via-web", $accessible_via_web);
-		$sentence->setAttribute("address", "0.0.0.0/0");
-		$this->talker->send($sentence);
-		return "Sucsess";
+	public function set_web_access($param) {
+		$this->_conn->comm("/ip/accounting/web-access/set", $param);
+		$this->_conn->disconnect();
+		return "Success";
+	}
+	
+	/**
+	 * This method is used to display all snapshot
+	 * @return type array
+	 */
+	public function get_all_snapshot() {
+		$array = $this->_conn->comm('/ip/accounting/snapshot/getall');
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
+			return "No Ip Accounting Snapshot To Set, Please Your Add Ip Accounting Snapshot";
+	}
+	
+	/**
+	 * This method is used to display all uncounted
+	 * @return type array
+	 */
+	public function get_all_uncounted() {
+		$array = $this->_conn->comm('/ip/accounting/uncounted/getall');
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
+			return "No Ip Accounting Uncounted To Set, Please Your Add Ip Accounting Uncounted";
 	}
 }
 
