80dd2c134f45d04a278d3e7c9e5498c02d15255a
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-05-30 02:26:33 +0700

    Ip_Firewall

--- modules/routeros/components/plugin/mikrotik/ip/MIpfirewall.php
+++ modules/routeros/components/plugin/mikrotik/ip/MIpfirewall.php
@@ -23,55 +23,66 @@ class MIpfirewall {
 	}
 	
 	/**
-	 * This method is used to add the firewall nat
-	 * @param type $param array
+	 * This method is used to display all firewall nat
+	 * @attr
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/NAT
+	 *
 	 * @return type array
-	 * 
+	 */
+	public function get_all_firewall_nat() {
+		$array = $this->_conn->comm("/ip/firewall/nat/getall");
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
+			return "No Ip Firewall NAT To Set, Please Your Add Ip Firewall NAT";
+	}
+	
+	/**
+	 * This method is used to add the firewall nat
+	 * @attr
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/NAT
+	 *
+	 * @return type array
 	 */
 	public function add_firewall_nat($param) {
-		$sentence = new SentenceUtil();
-	   $sentence->addCommand("/ip/firewall/nat/add");
-	   foreach ($param as $name => $value){
-			   $sentence->setAttribute($name, $value);
-	   }	   
-	   $this->talker->send($sentence);
-	   return "Sucsess";
+		$this->_conn->comm("/ip/firewall/nat/add", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
-	 * This method is used to disable firewall nat by id
-	 * @param type $id string
+	 * This method is used to enable firewall nat
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/NAT
+	 *
 	 * @return type array
-	 * 
 	 */
-	public function disable_firewall_nat($id) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/firewall/nat/disable");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";	  
+	public function enable_firewall_nat($param) {
+		$this->_conn->comm("/ip/firewall/nat/enable", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
-	 * This method is used to enable firewall nat by id
-	 * @param type $id string
+	 * This method is used to disable firewall nat
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/NAT
+	 *
 	 * @return type array
-	 * 
 	 */
-	public function enable_firewall_nat($id) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/firewall/nat/enable");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";	  
+	public function disable_firewall_nat($param) {
+		$this->_conn->comm("/ip/firewall/nat/disable", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
-	 * This method is used to change firewall nat based on the id
-	 * @param type $param array
-	 * @param type $id string
+	 * This method is used to change firewall nat
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/NAT
+	 *
 	 * @return type array
-	 * 
 	 */
 	public function set_firewall_nat($param, $id) {
 		$sentence = new SentenceUtil();
@@ -84,57 +95,49 @@ class MIpfirewall {
 		return "Sucsess";
 	}
 	
+	 /**
+	 * This method is used to display one firewall nat in detail
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/NAT
+	 *
+	 * @return type array
+	 */
+	public function detail_firewall_nat($param) {
+		$array = $this->_conn->comm("/ip/firewall/nat/print", $param);
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
+			return "No Ip Firewall NAT With This id = ".$param;
+	}
+	
 	/**
 	 * This method is used to remove firewall nat
-	 * @param type $id string
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/NAT
+	 *
 	 * @return type array
-	 * 
 	 */
-	public function delete_firewall_nat($id) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/firewall/nat/remove");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";	  
+	public function delete_firewall_nat($param) {
+		$this->_conn->comm("/ip/firewall/nat/remove", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
-	 * This method is used to display all firewall nat
+	 * This method is used to display all firewall filter
+	 * @attr
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Filter
+	 *
 	 * @return type array
-	 * 
 	 */
-	public function get_all_firewall_nat() {
-		$sentence = new SentenceUtil();
-		$sentence->fromCommand("/ip/firewall/nat/getall");
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0 ;
-		if ($i < $rs->size()){
-			return $rs->getResultArray();
-		}  else {
-			return "No Ip Firewall NAT To Set, Please Your Add Ip Firewall NAT";
-		}
-	}
-	
-	 /**
-	 * This method is used to display one firewall nat 
-	 * in detail based on the id
-	 * @param type $id string
-	 * @return type array
-	 * 
-	 */
-	public function detail_firewall_nat($id) {
-		$sentence = new SentenceUtil();
-		$sentence->fromCommand("/ip/firewall/nat/print");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0;
-		if($i < $rs->size()){
-			return $rs->getResultArray();
-		}  else {
-			return "No Ip Firewall NAT With This id = ".$id;
-		}
+	public function get_all_firewall_filter() {
+		$array = $this->_conn->comm("/ip/firewall/filter/getall");
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
+			return "No Ip Firewall Filter To Set, Please Your Add Ip Firewall Filter";
 	}
 	
 	/**
@@ -144,81 +147,43 @@ class MIpfirewall {
 	 * This method is used to add the firewall filter
 	 */
 	public function add_firewall_filter($param) {
-		$sentence = new SentenceUtil();
-	   $sentence->addCommand("/ip/firewall/filter/add");
-	   foreach ($param as $name => $value){
-			   $sentence->setAttribute($name, $value);
-	   }	   
-	   $this->talker->send($sentence);
-	   return "Sucsess";
+		$this->_conn->comm("/ip/firewall/filter/add", $param);
+		$this->_conn->disconnect();
+		return "Success";
+	}
+	
+	 /**
+	 * This method is used to enable firewall filter
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Filter
+	 *
+	 * @return type array
+	 */
+	public function enable_firewall_filter($param) {
+		$this->_conn->comm("/ip/firewall/filter/enable", $param);
+		$this->_conn->disconnect();
+		return "Success";
+	}
+	
+	 /**
+	 * This method is used to disable firewall filter
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Filter
+	 *
+	 * @return type array
+	 */
+	public function disable_firewall_filter($param) {
+		$this->_conn->comm("/ip/firewall/filter/disable", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
-	 * This method is used to display all firewall filter
+	 * This method is used to change firewall nat
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Filter
+	 *
 	 * @return type array
-	 * 
-	 */
-	public function get_all_firewall_filter() {
-		$sentence = new SentenceUtil();
-		$sentence->fromCommand("/ip/firewall/filter/getall");
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0 ;
-		if ($i < $rs->size()){
-			return $rs->getResultArray();
-		}  else {
-			return "No Ip Firewall Filter To Set, Please Your Add Ip Firewall Filter";
-		}
-	}
-	
-	 /**
-	 * This method is used to enable firewall filter by id
-	 * @param type $id string
-	 * @return type array
-	 * 
-	 */
-	public function enable_firewall_filter($id) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/firewall/filter/enable");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";   
-	}
-	
-	 /**
-	 * This method is used to disable firewall filter by id
-	 * @param type $id string
-	 * @return type array
-	 * 
-	 */
-	public function disable_firewall_filter($id) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/firewall/filter/disable");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";   
-	}
-	
-	 /**
-	 * This method is used to remove firewall filter by id
-	 * @param type $id string
-	 * @return type array
-	 * 
-	 */
-	public function delete_firewall_filter($id) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/firewall/filter/remove");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";   
-	}
-	
-	/**
-	 * This method is used to change firewall nat based on the id
-	 * @param type $param array
-	 * @param type $id string
-	 * @return type array
-	 * 
 	 */
 	public function set_firewall_filter($param, $id) {
 		$sentence = new SentenceUtil();
@@ -232,103 +197,94 @@ class MIpfirewall {
 	}
 	
 	 /**
-	 * This method is used to display one firewall filter
-	 * in detail based on the id
-	 * @param type $id string
+	 * This method is used to display one firewall filter in detail
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Filter
+	 *
 	 * @return type array
-	 * 
 	 */
-	public function detail_firewall_filter($id) {
-		$sentence = new SentenceUtil();
-		$sentence->fromCommand("/ip/firewall/filter/print");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0;
-		if($i < $rs->size()){
-			return $rs->getResultArray();
-		}  else {
-			return "No Ip Firewall Filter With This id = ".$id;
-		}
+	public function detail_firewall_filter($param) {
+		$array = $this->_conn->comm("/ip/firewall/filter/print", $param);
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
+			return "No Ip Firewall Filter With This id = ".$param;
+	}
+	
+	 /**
+	 * This method is used to remove firewall filter
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Filter
+	 *
+	 * @return type array
+	 */
+	public function delete_firewall_filter($param) {
+		$this->_conn->comm("/ip/firewall/filter/remove", $param);
+		$this->_conn->disconnect();
+		return "Success";
+	}
+	
+	/**
+	 * This method used for get all Ip Firewall Mangle
+	 * @attr
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Mangle
+	 *
+	 * @return type array
+	 */
+	public function get_all_firewall_mangle() {
+		$array = $this->_conn->comm("/ip/firewall/mangle/getall");
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
+			return "No Ip Firewall Mangle To Set, Please Your Add Ip Firewall Mangle";
 	}
 	
 	/**
 	 * This method used for add new Ip Firewall Mangle 
-	 * @param type $param array
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Mangle
+	 *
 	 * @return type array
 	 */
 	public function add_firewall_mangle($param) {
-		$sentence = new SentenceUtil();
-	   $sentence->addCommand("/ip/firewall/mangle/add");
-	   foreach ($param as $name => $value){
-			   $sentence->setAttribute($name, $value);
-	   }	   
-	   $this->talker->send($sentence);
-	   return "Sucsess";
-	}
-	
-	/**
-	 * This method used for disable Ip Firewall Mangle
-	 * @param type $id string
-	 * @return type array
-	 */
-	public function disable_firewall_mangle($id) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/firewall/mangle/disable");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";   
+		$this->_conn->comm("/ip/firewall/mangle/add", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
 	 * This method used for enable Ip Firewall Mangle
-	 * @param type $id string
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Mangle
+	 *
 	 * @return type array
 	 */
-	public function enable_firewall_mangle($id) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/firewall/mangle/enable");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";   
+	public function enable_firewall_mangle($param) {
+		$this->_conn->comm("/ip/firewall/mangle/enable", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
-	 * This method used for delete Ip Firewall Mangle
-	 * @param type $id string
+	 * This method used for disable Ip Firewall Mangle
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Mangle
+	 *
 	 * @return type array
 	 */
-	public function delete_firewall_mangle($id) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/firewall/mangle/remove");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";   
-	}
-	
-	/**
-	 * This method used for detail Ip Firewall Mangle
-	 * @param type $id string
-	 * @return type array
-	 */
-	public function detail_firewall_mangle($id) {
-		 $sentence = new SentenceUtil();
-		$sentence->fromCommand("/ip/firewall/mangle/print");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0;
-		if($i < $rs->size()){
-			return $rs->getResultArray();
-		}  else {
-			return "No Ip Firewall Mangle With This id = ".$id;
-		}
+	public function disable_firewall_mangle($param) {
+		$this->_conn->comm("/ip/firewall/mangle/disable", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
 	 * This method used for set or edit Ip Firewall Mangle
-	 * @param type $param array
-	 * @param type $id string
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Mangle
+	 *
 	 * @return type array
 	 */
 	public function set_firewall_mangle($param, $id) {
@@ -343,113 +299,119 @@ class MIpfirewall {
 	}
 	
 	/**
-	 * This method used for get all Ip Firewall Mangle
+	 * This method used for detail Ip Firewall Mangle
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Mangle
+	 *
 	 * @return type array
 	 */
-	public function get_all_firewall_mangle() {
-		$sentence = new SentenceUtil();
-		$sentence->fromCommand("/ip/firewall/mangle/getall");
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0 ;
-		if ($i < $rs->size()){
-			return $rs->getResultArray();
-		}  else {
-			return "No Ip Firewall Mangle To Set, Please Your Add Ip Firewall Mangle";
-		}		
+	public function detail_firewall_mangle($param) {
+		$array = $this->_conn->comm("/ip/firewall/mangle/print", $param);
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
+			return "No Ip Firewall Mangle With This id = ".$param;
+	}
+	
+	/**
+	 * This method used for delete Ip Firewall Mangle
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Mangle
+	 *
+	 * @return type array
+	 */
+	public function delete_firewall_mangle($param) {
+		$this->_conn->comm("/ip/firewall/mangle/remove", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
 	 * This method used for get all firewall connection
+	 * @attr
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Connection_tracking
+	 *
 	 * @return type array
 	 */
 	public function get_all_firewall_connection() {
-		$sentence = new SentenceUtil();
-		$sentence->fromCommand("/ip/firewall/connection/getall");
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0 ;
-		if ($i < $rs->size()){
-			return $rs->getResultArray();
-		}  else {
+		$array = $this->_conn->comm("/ip/firewall/connection/getall");
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
 			return "No Ip Firewall Connection To Set, Please Your Add Ip Firewall Connection";
-		}		
 	}
 	
 	/**
 	 * This method used for delete firewall connection
-	 * @param type $id string
+	 * @attr
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Firewall/Connection_tracking
+	 *
 	 * @return type array
 	 */
-	public function delete_firewall_connection($id) {
-		 $sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/firewall/connection/remove");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";   
+	public function delete_firewall_connection($param) {
+		$this->_conn->comm("/ip/firewall/connection/remove", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
 	 * This method used for get all Ip Firewall Service Port
-	 * @return type arrray
-	 */
-	public function get_all_firewall_service_port() {
-		 $sentence = new SentenceUtil();
-		$sentence->fromCommand("/ip/firewall/service-port/getall");
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0 ;
-		if ($i < $rs->size()){
-			return $rs->getResultArray();
-		}  else {
-			return "No Ip Firewall service-port To Set, Please Your Add Ip Firewall service-port";
-		}
-	}
-	
-	/**
-	 * This method used for disable Ip Firewall Service Port
-	 * @param type $id string
+	 * @attr
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Services#Service_Ports
+	 *
 	 * @return type array
 	 */
-	public function disable_firewall_service_port($id) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/firewall/service-port/disable");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";   
+	public function get_all_firewall_service_port() {
+		$array = $this->_conn->comm("/ip/firewall/service-port/getall");
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
+			return "No Ip Firewall service-port To Set, Please Your Add Ip Firewall service-port";
 	}
 	
 	/**
 	 * This method used for enable Ip Firewall Service Port
-	 * @param type $id string
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Services#Service_Ports
+	 *
 	 * @return type array
 	 */
-	public function enable_firewall_service_port($id) {
-	   $sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/firewall/service-port/enable");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";   
+	public function enable_firewall_service_port($param) {
+		$this->_conn->comm("/ip/firewall/service-port/enable", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
+	 * This method used for disable Ip Firewall Service Port
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Services#Service_Ports
 	 *
-	 * @param type $id string 
 	 * @return type array
-	 * 
 	 */
-	public function detail_firewall_service_port($id) {
-		$sentence = new SentenceUtil();
-		$sentence->fromCommand("/ip/firewall/service-port/print");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0;
-		if($i < $rs->size()){
-			return $rs->getResultArray();
-		}  else {
-			return "No Ip Firewall Service-Port With This id = ".$id;
-		}
+	public function disable_firewall_service_port($param) {
+		$this->_conn->comm("/ip/firewall/service-port/disable", $param);
+		$this->_conn->disconnect();
+		return "Success";
+	}
+	
+	/**
+	 * This method used for detail Firewall Service Port
+	 * @param
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/Services#Service_Ports
+	 *
+	 * @return type array
+	 */
+	public function detail_firewall_service_port($param) {
+		$array = $this->_conn->comm("/ip/firewall/service-port/print", $param);
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
+			return "No Ip Firewall Service-Port With This id = ".$param;
 	}
 	
 }
