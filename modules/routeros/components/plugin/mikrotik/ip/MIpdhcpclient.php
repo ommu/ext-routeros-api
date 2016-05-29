83fc24066c3ae496b1c8ad5538a6a9b1f11d790b
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-05-29 14:29:51 +0700

    ip dhcp client fixed

--- modules/routeros/components/plugin/mikrotik/ip/MIpdhcpclient.php
+++ modules/routeros/components/plugin/mikrotik/ip/MIpdhcpclient.php
@@ -2,6 +2,18 @@
 /**
  * Description of Mapi_Ip_Dhcp_Client
  *
+ * TOC :
+ *	get_all_address
+ *	get_all_dhcp_client
+ *	add_dhcp_client
+ *	enable_dhcp_client
+ *	disable_dhcp_client
+ *	renew_dhcp_client
+ *	release_dhcp_client
+ *	set_dhcp_client
+ *	detail_dhcp_client
+ *	delete_dhcp_client 
+ *
  * @author Putra Sudaryanto <putra.sudaryanto@gmail.com> <putra@sudaryanto.id>
  * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
  * @created date 26 May 2016, 15:15 WIB
@@ -21,72 +33,99 @@ class MIpdhcpclient{
 		$this->talker = $talker;
 		$this->_conn = $conn;
 	}
+
+	/**
+	 * This method is used to display all dhcp client
+	 * @return type array
+	 */
+	public function get_all_dhcp_client() {
+		$array = $this->_conn->comm("/ip/dhcp-client/getall");
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
+			return "No Ip Dhcp-Client To Set, Please Your Add Ip Dhcp-Client";
+	}
 	
 	/**
 	 * This method is used to add dhcp client
-	 * @param type $param array
+	 * @param
+	 *	interface (string; Default: ): Interface on which DHCP client will be running.
+	 *		use-peer-dns (yes | no; Default: yes): Whether to accept the DNS settings advertised by DHCP Server. (Will override the settings put in the /ip dns submenu.
+	 *		use-peer-ntp (yes | no; Default: yes): Whether to accept the NTP settings advertised by DHCP Server. (Will override the settings put in the /system ntp client submenu)
+	 *	add-default-route (yes | no | special-classless; Default: yes): Whether to install default route in routing table received from dhcp server. By default RouterOS client complies to RFC and ignores option 3 if classless option 121 is received. To force client not to ignore option 3 set special-classless. This parameter is available in v6rc12+
+	 *		yes - adds classless route if received, if not then add default route (old behavior)
+	 *		special-classless - adds both classless route if received and default route (MS style)
+	 *	default-route-distance (integer:0..255; Default: ): Distance of default route. Applicable if add-default-route is set to yes.
+	 *	client-id (string; Default: ): Corresponds to the settings suggested by the network administrator or ISP. If not specified, client's MAC address will be sent
+	 *	comment (string; Default: ): Short description of the client
+	 *	disabled (yes | no; Default: yes)	
+	 *	host-name (string; Default: ): Host name of the client sent to a DHCP server. If not specified, client's system identity will be used.	 
+	 *
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/DHCP_Client
+	 *
 	 * @return type array
 	 */
 	public function add_dhcp_client($param) {
-	   $sentence = new SentenceUtil();
-	   $sentence->addCommand("/ip/dhcp-client/add");
-	   foreach ($param as $name => $value){
-			   $sentence->setAttribute($name, $value);
-	   }	   
-	   $this->talker->send($sentence);
-	   return "Sucsess";
+		$this->_conn->comm("/ip/dhcp-client/add", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
-	 * This method is used to disable dhcp client by id
-	 * @param type $id string
+	 * This method is used to enable dhcp client
+	 * @param
+	 *	interface, use-peer-dns, use-peer-ntp, add-default-route, default-route-distance, client-id, comment, disabled, host-name
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/DHCP_Client
+	 *
 	 * @return type array
 	 */
-	public function disable_dhcp_client($id) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/dhcp-client/disable");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";
+	public function enable_dhcp_client($param) {
+		$this->_conn->comm("/ip/dhcp-client/enable", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
-	 * This method is used to enable dhcp client by id
-	 * @param type $id string
+	 * This method is used to disable dhcp client
+	 * @param
+	 *	interface, use-peer-dns, use-peer-ntp, add-default-route, default-route-distance, client-id, comment, disabled, host-name
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/DHCP_Client
+	 *
 	 * @return type array
 	 */
-	public function enable_dhcp_client($id) {
-		 $sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/dhcp-client/enable");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";
+	public function disable_dhcp_client($param) {
+		$this->_conn->comm("/ip/dhcp-client/disable", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
-	 * This method is used to renew dhcp client  by id
-	 * @param type $id string
+	 * This method is used to renew dhcp client
+	 * @param
+	 *	interface, use-peer-dns, use-peer-ntp, add-default-route, default-route-distance, client-id, comment, disabled, host-name
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/DHCP_Client
+	 *
 	 * @return type array
 	 */
-	public function renew_dhcp_client($id) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/dhcp-client/renew");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";
+	public function renew_dhcp_client($param) {
+		$this->_conn->comm("/ip/dhcp-client/renew", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
-	 * This method is used to release dhcp client by id
-	 * @param type $id string
+	 * This method is used to release dhcp client
+	 * @param
+	 *	interface, use-peer-dns, use-peer-ntp, add-default-route, default-route-distance, client-id, comment, disabled, host-name
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/DHCP_Client
+	 *
 	 * @return type array
 	 */
-	public function release_dhcp_client($id) {
-		$sentence = new SentenceUtil();
-		$sentence->addCommand("/ip/dhcp-client/release");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		return "Sucsess";
+	public function release_dhcp_client($param) {
+		$this->_conn->comm("/ip/dhcp-client/release", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 	
 	/**
@@ -107,53 +146,34 @@ class MIpdhcpclient{
 	}
 	
 	/**
-	 * This method is used to remove dhcp client by id
-	 * @param type $id string
+	 * This method is used to display one ip dhcp client in detail
+	 * @param
+	 *	interface, use-peer-dns, use-peer-ntp, add-default-route, default-route-distance, client-id, comment, disabled, host-name
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/DHCP_Client
+	 *
 	 * @return type array
 	 */
-	public function delete_dhcp_client($id) {
-	   $sentence = new SentenceUtil();
-	   $sentence->addCommand("/ip/dhcp-client/remove");
-	   $sentence->where(".id", "=", $id);
-	   $enable = $this->talker->send($sentence);
-	   return "Sucsess";
-	}
-
-	/**
-	 * This method is used to display all dhcp client
-	 * @return type array
-	 */
-	public function get_all_dhcp_client() {
-		$sentence = new SentenceUtil();
-		$sentence->fromCommand("/ip/dhcp-client/getall");
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0 ;
-		if ($i < $rs->size()){
-			return $rs->getResultArray();
-		}  else {
-			return "No Ip Dhcp-Client To Set, Please Your Add Ip Dhcp-Client";
-		}
+	public function detail_dhcp_client($param) {
+		$array = $this->_conn->comm("/ip/dhcp-client/print", $param);
+		$this->_conn->disconnect();
+		if(0 < count($array))
+			return $array;
+		else
+			return "No Ip Dhcp-Client With This id = ".$param;
 	}
 	
 	/**
-	 * This method is used to display one ip dhcp client
-	 * in detail based on the id
-	 * @param type $id string
+	 * This method is used to remove dhcp client
+	 * @param
+	 *	interface, use-peer-dns, use-peer-ntp, add-default-route, default-route-distance, client-id, comment, disabled, host-name
+	 *	URL: http://wiki.mikrotik.com/wiki/Manual:IP/DHCP_Client
+	 *
 	 * @return type array
 	 */
-	public function detail_dhcp_client($id) {
-		  $sentence = new SentenceUtil();
-		$sentence->fromCommand("/ip/dhcp-client/print");
-		$sentence->where(".id", "=", $id);
-		$this->talker->send($sentence);
-		$rs = $this->talker->getResult();
-		$i = 0;
-		if($i < $rs->size()){
-			return $rs->getResultArray();
-		}  else {
-			return "No Ip Dhcp-Client With This id = ".$id;
-		}
+	public function delete_dhcp_client($param) {
+		$this->_conn->comm("/ip/dhcp-client/remove", $param);
+		$this->_conn->disconnect();
+		return "Success";
 	}
 }
 
