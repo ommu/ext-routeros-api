13156ded76c2a7c4db2fb30ba5471d9f0f0cf30e
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-01-07 01:55:55 +0700

    commit

--- ORouterosAPI.php
+++ ORouterosAPI.php
@@ -3,10 +3,10 @@
  * ORouterosAPI class file.
  *
  * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
  * @copyright Copyright (c) 2016 Ommu Platform (opensource.ommu.co)
  * @created date 26 May 2016, 15:15 WIB
  * @link https://github.com/ommu/ommu-routeros-api
- * @contact (+62)856-299-4114
  */
 
 defined('MIKROTIK_PATH') or define('MIKROTIK_PATH',dirname(__FILE__).DIRECTORY_SEPARATOR.'mikrotik'.DIRECTORY_SEPARATOR);
--- mikrotik/tool/OTool.php
+++ mikrotik/tool/OTool.php
@@ -9,10 +9,10 @@
  *	ppp_aaa
  *
  * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
  * @copyright Copyright (c) 2016 Ommu Platform (opensource.ommu.co)
  * @created date 26 May 2016, 15:15 WIB
  * @link https://github.com/ommu/ommu-routeros-api
- * @contact (+62)856-299-4114
  */
  
 defined('TOOL_PATH') or define('TOOL_PATH',dirname(__FILE__).DIRECTORY_SEPARATOR);
